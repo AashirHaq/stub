129b2f08085a08ac65f75c11a1694a6b451bed37
Aashir Haq <aashirulhaque@gmail.com>
2021-09-23 12:39:01 +0500

    add compatibility only for php 7.4

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Aashirhaq\Stub\Tests\Feature;
+
+use Aashirhaq\Stub\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
